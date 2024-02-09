5f1f316373e948c14e8198e3685f1752dd478ab6
mehul <mehulmanhas26@gmail.com>
2024-02-10 02:03:23 +0530

    Adding base project files with dependencies

---
+++ src/main/java/com/mehulmanhas/UpstoxTradingAutomation/UpstoxTradingAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.mehulmanhas.UpstoxTradingAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpstoxTradingAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpstoxTradingAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mehulmanhas/UpstoxTradingAutomation/UpstoxTradingAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehulmanhas.UpstoxTradingAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpstoxTradingAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
